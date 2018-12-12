09db9a74d9fa145a2ed7e7a8830941a57591b7b6
pag-r <damian.lubawy@gmail.com>
2018-12-12 20:09:13 +0100

    Added POST as easier way to execute commands, small fixes in code

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ## Simple Go-based webshell for Docker
-By default http server listens on port 9090. Commands have to be seperared by comma symbol i.e. `ls,-al,|,grep,Dockerfile`
+By default http server listens on port 9090. <br />
+If using GET commands have to be seperared by comma symbol i.e. `ls,-al,|,grep,Dockerfile`<br />
+If using POST format: 
+```json
+{"command" : "ls -al | grep Dockerfile"}
+```
 #### Build Go binary
 ```sh
 go build -o run run.go
@@ -13,15 +18,29 @@ docker build --tag webshell .
 docker run -it --rm -p 9090:9090 --name webshell webshell
 ```
 #### Usage
+1. GET
 - Shell command output:
 ```sh
-curl <localhost>:9090/cat,run.py
+curl -XGET <localhost>:9090/cat,run.py
 ```
 - Python3 script execution
 ```sh
-curl <localhost>:9090/python3,run.py
+curl -XGET <localhost>:9090/python3,run.py
 ```
 - Python3 direct command execution
 ```sh
-curl <localhost>:9090/python3,-c,'print("foo")'
+curl -XGET <localhost>:9090/python3,-c,'print("foo")'
+```
+2. POST
+- Shell command output:
+```sh
+curl -XPOST <localhost>:9090/ -d '{"command" : "cat run.py"}'
+```
+- Python3 script execution
+```sh
+curl -XPOST <localhost>:9090/ -d '{"command" : "python3 run.py"}'
+```
+- Python3 direct command execution
+```sh
+curl -XPOST <localhost>:9090/ -d '{"command":"python3 -c \"print(\\\"foo\\\")\" "}'
 ```
--- run.go
+++ run.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os/exec"
 	"strings"
 )
 
-func commandExecute(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	fmt.Println("path", r.URL.Path)
+var command string
+
+// CommandJSON returns string with command from JSON
+type CommandJSON struct {
+	Cmd string `json:"command"`
+}
+
+// ServeGET formats comma separated string to command string
+func ServeGET(r *http.Request) (string, error) {
 	GETCommand := strings.Replace(r.URL.Path, "/", "", 1)
-	fmt.Printf("%s\n", GETCommand)
 	len := len(GETCommand)
 	var args []string
 	var command string
@@ -24,20 +30,56 @@ func commandExecute(w http.ResponseWriter, r *http.Request) {
 	command = fmt.Sprintf("/usr/bin/which")
 	out, err := exec.Command(command, args[0]).Output()
 	if err != nil {
-		fmt.Printf("Error in which command: %s", err)
-		fmt.Fprintf(w, "Error in which command: %s", err)
+		return "", err
 	}
 	realCommand := strings.Split(string(out), "\n")[0]
 	args[0] = realCommand
-	toExecute := fmt.Sprintf("%s", strings.Join(args, " "))
-	fmt.Printf("RealCommand:\t%s\nBuildCommand:\t%s\nStarArgs:\t%s\nRealArgs:\t%s\n", realCommand, toExecute, args, GETCommand)
-	fmt.Fprintf(w, "RealCommand:\t%s\nBuildCommand:\t%s\nStarArgs:\t%s\nRealArgs:\t%s\n", realCommand, toExecute, args, GETCommand)
-	outCommand, err := exec.Command("bash", "-c", toExecute).Output()
-	if err != nil {
-		fmt.Printf("Error in exec: %s", err)
-		fmt.Fprintf(w, "Error in exec: %s", err)
+	return fmt.Sprintf("%s", strings.Join(args, " ")), nil
+}
+
+// ServePOST unmarshalls string from JSON POST
+func ServePOST(r *http.Request) (string, error) {
+	var comStruct CommandJSON
+	for key := range r.Form {
+		err := json.Unmarshal([]byte(key), &comStruct)
+		if err != nil {
+			return "", err
+		}
 	}
-	fmt.Fprintf(w, "\nResult:\n\n%s\n", outCommand)
+	return comStruct.Cmd, nil
+}
+
+func commandExecute(w http.ResponseWriter, r *http.Request) {
+	var command string
+	var err error
+	r.ParseForm()
+	if r.Method == "GET" {
+		command, err = ServeGET(r)
+		if err != nil {
+			errorString := fmt.Sprintf("\nError while executing which command: %s\n", err)
+			fmt.Print(errorString)
+			fmt.Fprint(w, errorString)
+		}
+	} else if r.Method == "POST" {
+		command, err = ServePOST(r)
+		if err != nil {
+			errorString := fmt.Sprintf("\nError while parsing POST data: %s\n", err)
+			fmt.Print(errorString)
+			fmt.Fprint(w, errorString)
+		}
+	}
+
+	outputHeader := fmt.Sprintf("Command:\t%s\n", command)
+	outCommand, err := exec.Command("bash", "-c", command).Output()
+	if err != nil {
+		errorString := fmt.Sprintf("\nError in exec: %s\n", err)
+		fmt.Printf(errorString)
+		fmt.Fprintf(w, errorString)
+	}
+	formattedOutput := fmt.Sprintf(
+		"\nMethod:\t\t%s\n%sResult:\n\n%s\n", r.Method, outputHeader, string(outCommand))
+	fmt.Print(formattedOutput)
+	fmt.Fprintf(w, formattedOutput)
 }
 
 func main() {
